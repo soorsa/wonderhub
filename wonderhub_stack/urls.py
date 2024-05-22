de223b51101eb76e561d849c0ef477894424cdc1
soorsa <kingsoorsa@gmail.com>
2024-05-22 02:08:13 +0100

    4th commit

--- wonderhub_stack/urls.py
+++ wonderhub_stack/urls.py
@@ -18,6 +18,7 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -32,4 +33,8 @@ urlpatterns = [
 
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+render = True
+if render:
+    urlpatterns += staticfiles_urlpatterns()
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
