241278d263c99f549a82014acbc4c30b2f6dc7b4
soorsa <kingsoorsa@gmail.com>
2024-05-29 14:01:22 +0100

    version 0.0.6(test-subdomain)

--- wonderhub_stack/hosts.py
+++ wonderhub_stack/hosts.py
@@ -8,7 +8,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'www', 'wonderhub_stack.urls', name='www'),
+    host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'academy', 'blog.urls', name='academy'),
 )
 # if settings.RENDER:
