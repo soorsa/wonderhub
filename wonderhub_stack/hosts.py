aa96c5b0f4ec8d734ffb8f4a06717232fed3b9c3
soorsa <kingsoorsa@gmail.com>
2024-05-29 13:18:43 +0100

    version 0.0.6(test-subdomain)

--- wonderhub_stack/hosts.py
+++ wonderhub_stack/hosts.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django_hosts import patterns, host
 
 host_patterns = patterns(
@@ -5,3 +11,5 @@ host_patterns = patterns(
     host(r'www', 'wonderhub_stack.urls', name='www'),
     host(r'academy', 'blog.urls', name='academy'),
 )
+if settings.RENDER:
+    urlpatterns += staticfiles_urlpatterns()
